3d4514791e024919e325927888eb76d7561dc85d
Dmitriy Din <dmitriy1d01@gmail.com>
2026-08-10 12:56:01 +0300

    🛠️ Added scripts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig([
   {
-    entry: ["./src/index.ts", "./src/analytics/index.ts"],
+    entry: [
+      "./src/index.ts",
+      "./src/analytics/index.ts",
+      "./src/vault/index.ts",
+    ],
     platform: "neutral",
     dts: true,
   },
